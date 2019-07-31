eeaf7080a1edd75a9023eeea0ab4b1d21c7e21cb
Washim Ahmed <washim.ahmed@gmail.com>
2019-07-31 11:02:21 +0530

    documentation

--- docs/_build/html/_sources/index.rst.txt
+++ docs/_build/html/_sources/index.rst.txt
@@ -1,15 +1,13 @@
 Welcome to GorillaML's documentation!
 =====================================
+This is the application which allow individual, organization, developer, publisher to manage, publish, monitor webservices, machine learning api, custom forms and many more things collaborately.
 
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
 
+Installation
+=====================================
+.. code-block:: shell
 
-
-Indices and tables
-==================
-
-* :ref:`genindex`
-* :ref:`modindex`
-* :ref:`search`
+    pip install gorillaml
--- docs/conf.py
+++ docs/conf.py
@@ -22,6 +22,7 @@ copyright = '2019, Washim Ahmed'
 author = 'Washim Ahmed'
 
 # The full version, including alpha/beta/rc tags
+version = 'Development'
 release = 'dev-0.x-0.1'
 
 
@@ -30,8 +31,7 @@ release = 'dev-0.x-0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -48,6 +48,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
+html_theme_path = ['_themes']
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
